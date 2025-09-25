0ded939cbd14b55c0eb9f8858a7d7c01b1b3d650
Mick <mickjagger19@icloud.com>
2025-09-25 17:18:04 +0800

    upd

--- python/sglang/srt/layers/rotary_embedding.py
+++ python/sglang/srt/layers/rotary_embedding.py
@@ -1204,7 +1204,6 @@ class MRotaryEmbedding(RotaryEmbedding):
                         "qwen2_vl",
                         "qwen3_vl",
                         "qwen3_vl_moe",
-                        "qwen3_omni_moe",
                     ):
                         t_index = (
                             torch.arange(llm_grid_t)
